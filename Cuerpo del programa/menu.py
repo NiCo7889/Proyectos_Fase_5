ec14601c9efa6fd1a9c25a02e2199a8ab116e6c2
NiCo7889 <113980882+NiCo7889@users.noreply.github.com>
2023-03-14 13:28:16 +0100

    algunos cambios en el menu

--- Cuerpo del programa/menu.py
+++ Cuerpo del programa/menu.py
@@ -23,13 +23,13 @@ def iniciar(): # Función que inicia el programa y muestra el menú
 
         if opcion == '1':
             print("Listando los clientes...\n")
-            for cliente in db.Clientes.lista:
+            for cliente in db.Inventario.lista:
                 print(cliente)
 
         elif opcion == '2':
             print("Buscando un cliente...\n")
             dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
-            cliente = db.Clientes.buscar(dni)
+            cliente = db.Inventario.buscar(dni)
             print(cliente) if cliente else print("Cliente no encontrado.")
 
         elif opcion == '3':
@@ -38,24 +38,24 @@ def iniciar(): # Función que inicia el programa y muestra el menú
             dni = None
             while True:
                 dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
-                if helpers.dni_valido(dni, db.Clientes.lista):
+                if helpers.dni_valido(dni, db.Inventario.lista):
                     break
 
             nombre = helpers.leer_texto(2, 30, "Nombre (de 2 a 30 chars)").capitalize()
             apellido = helpers.leer_texto(2, 30, "Apellido (de 2 a 30 chars)").capitalize()
-            db.Clientes.crear(dni, nombre, apellido)
+            db.Inventario.crear(dni, nombre, apellido)
             print("Cliente añadido correctamente.")
 
         elif opcion == '4':
             print("Modificando un cliente...\n")
             dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
-            cliente = db.Clientes.buscar(dni)
+            cliente = db.Inventario.buscar(dni)
             if cliente:
                 nombre = helpers.leer_texto(
                     2, 30, f"Nombre (de 2 a 30 chars) [{cliente.nombre}]").capitalize()
                 apellido = helpers.leer_texto(
                     2, 30, f"Apellido (de 2 a 30 chars) [{cliente.apellido}]").capitalize()
-                db.Clientes.modificar(cliente.dni, nombre, apellido)
+                db.Inventario.modificar(cliente.dni, nombre, apellido)
                 print("Cliente modificado correctamente.")
             else:
                 print("Cliente no encontrado.")
@@ -63,7 +63,7 @@ def iniciar(): # Función que inicia el programa y muestra el menú
         elif opcion == '5':
             print("Borrando un cliente...\n")
             dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
-            print("Cliente borrado correctamente.") if db.Clientes.borrar(
+            print("Cliente borrado correctamente.") if db.Inventario.borrar(
                 dni) else print("Cliente no encontrado.")
 
         elif opcion == '6':
