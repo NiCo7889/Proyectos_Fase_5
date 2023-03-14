0af0159fe87c537b355540b8e5296b44ec352b62
NiCo7889 <113980882+NiCo7889@users.noreply.github.com>
2023-03-14 13:14:39 +0100

    csvs

--- Cuerpo del programa/config.py
+++ Cuerpo del programa/config.py
@@ -2,8 +2,8 @@
 import sys
 
 # Create a variable to store the database path
-DATABASE_PATH = "clientes.csv"
+DATABASE_PATH = "inventario.csv"
 
 # If the current script is being run by pytest, change the database path
 if "pytest" in sys.argv[0]:
-    DATABASE_PATH = "tests/clientes_test.csv"
+    DATABASE_PATH = "tests/inventario_test.csv"
