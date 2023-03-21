78d2f24b905d891914efde2e60247af1935f0372
NiCo7889 <113980882+NiCo7889@users.noreply.github.com>
2023-03-21 14:13:00 +0100

    menu

--- Cuerpo del programa/menu.py
+++ Cuerpo del programa/menu.py
@@ -22,29 +22,29 @@ def iniciar(): # Función que inicia el programa y muestra el menú
         helpers.limpiar_pantalla()
 
         if opcion == '1':
-            print("Listando los clientes...\n")
-            for cliente in db.Inventario.lista:
-                print(cliente)
+            print("Listando los productos...\n")
+            for producto in db.Inventario.lista:
+                print(producto)
 
         elif opcion == '2':
-            print("Buscando un cliente...\n")
-            dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
-            cliente = db.Inventario.buscar(dni)
-            print(cliente) if cliente else print("Cliente no encontrado.")
+            print("Buscando un producto...\n")
+            CD = helpers.leer_texto(3, 3, "CD (2 int y 1 char)").upper()
+            producto = db.Inventario.buscar(CD)
+            print(cliente) if cliente else print("Producto no encontrado.")
 
         elif opcion == '3':
-            print("Añadiendo un cliente...\n")
+            print("Añadiendo un producto...\n")
 
             dni = None
             while True:
-                dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
+                dni = helpers.leer_texto(3, 3, "CD (2 int y 1 char)").upper()
                 if helpers.dni_valido(dni, db.Inventario.lista):
                     break
 
             nombre = helpers.leer_texto(2, 30, "Nombre (de 2 a 30 chars)").capitalize()
             apellido = helpers.leer_texto(2, 30, "Apellido (de 2 a 30 chars)").capitalize()
             db.Inventario.crear(dni, nombre, apellido)
-            print("Cliente añadido correctamente.")
+            print("Producto añadido correctamente.")
 
         elif opcion == '4':
             print("Modificando un cliente...\n")
@@ -61,10 +61,10 @@ def iniciar(): # Función que inicia el programa y muestra el menú
                 print("Cliente no encontrado.")
 
         elif opcion == '5':
-            print("Borrando un cliente...\n")
-            dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
-            print("Cliente borrado correctamente.") if db.Inventario.borrar(
-                dni) else print("Cliente no encontrado.")
+            print("Borrando un producto...\n")
+            dni = helpers.leer_texto(3, 3, "CD (2 int y 1 char)").upper()
+            print("Producto borrado correctamente.") if db.Inventario.borrar(
+                dni) else print("Producto no encontrado.")
 
         elif opcion == '6':
             print("Saliendo...\n")
