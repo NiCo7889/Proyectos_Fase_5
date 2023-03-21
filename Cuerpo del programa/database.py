f2f0b9396958e3dc6828563925905c7ef575f3be
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-21 16:01:09 +0100

    cambiando databse

--- Cuerpo del programa/database.py
+++ Cuerpo del programa/database.py
@@ -2,54 +2,54 @@ import csv # Importamos el módulo csv para poder leer y escribir en ficheros cs
 import config # Importamos el módulo config para poder acceder a la variable DATABASE_PATH
 
 
-class Cliente: # Creamos la clase Cliente para poder crear objetos de tipo Cliente con los atributos dni, nombre y apellido
-    def __init__(self, dni, nombre, apellido): # Creamos el método __init__ para inicializar los atributos de la clase
-        self.dni = dni
+class Producto: # Creamos la clase Cliente para poder crear objetos de tipo Cliente con los atributos dni, nombre y apellido
+    def __init__(self, CD, nombre, apellido): # Creamos el método __init__ para inicializar los atributos de la clase
+        self.CD = CD
         self.nombre = nombre
         self.apellido = apellido
 
     def __str__(self): # Creamos el método __str__ para poder imprimir los objetos de tipo Cliente
-        return f"({self.dni}) {self.nombre} {self.apellido}"
+        return f"({self.CD}) {self.nombre} {self.apellido}"
 
     def to_dict(self): # Creamos el método to_dict para poder convertir los objetos de tipo Cliente en diccionarios
-        return {'dni': self.dni, 'nombre': self.nombre, 'apellido': self.apellido}
+        return {'dni': self.CD, 'nombre': self.nombre, 'apellido': self.apellido}
 
 
-class Clientes: # Creamos la clase Clientes para poder crear objetos de tipo Clientes con los atributos lista
+class Inventario: # Creamos la clase Clientes para poder crear objetos de tipo Clientes con los atributos lista
 
     lista = []
     with open(config.DATABASE_PATH, newline='\n') as fichero:
         reader = csv.reader(fichero, delimiter=';')
-        for dni, nombre, apellido in reader:
-            cliente = Cliente(dni, nombre, apellido)
+        for CD, nombre, apellido in reader:
+            cliente = Cliente(CD, nombre, apellido)
             lista.append(cliente)
 # utilizo los decoradores @staticmethod para poder acceder a los métodos sin necesidad de crear un objeto de tipo Clientes
     @staticmethod
-    def buscar(dni):
+    def buscar(CD):
         for cliente in Clientes.lista:
-            if cliente.dni == dni:
+            if cliente.CD == CD:
                 return cliente
 
     @staticmethod
-    def crear(dni, nombre, apellido):
-        cliente = Cliente(dni, nombre, apellido)
+    def crear(CD, nombre, apellido):
+        cliente = Cliente(CD, nombre, apellido)
         Clientes.lista.append(cliente)
         Clientes.guardar()
         return cliente
 
     @staticmethod
-    def modificar(dni, nombre, apellido):
+    def modificar(CD, nombre, apellido):
         for indice, cliente in enumerate(Clientes.lista):
-            if cliente.dni == dni:
+            if cliente.CD == CD:
                 Clientes.lista[indice].nombre = nombre
                 Clientes.lista[indice].apellido = apellido
                 Clientes.guardar()
                 return Clientes.lista[indice]
 
     @staticmethod
-    def borrar(dni):
+    def borrar(CD):
         for indice, cliente in enumerate(Clientes.lista):
-            if cliente.dni == dni:
+            if cliente.CD == CD:
                 cliente = Clientes.lista.pop(indice)
                 Clientes.guardar()
                 return cliente
@@ -59,4 +59,4 @@ class Clientes: # Creamos la clase Clientes para poder crear objetos de tipo Cli
         with open(config.DATABASE_PATH, 'w', newline='\n') as fichero:
             writer = csv.writer(fichero, delimiter=';')
             for cliente in Clientes.lista:
-                writer.writerow((cliente.dni, cliente.nombre, cliente.apellido))
+                writer.writerow((cliente.CD, cliente.nombre, cliente.apellido))
